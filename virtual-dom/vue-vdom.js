f583e7d6ff0af9271e7a6caecbcbd7e3afd45d3c
fabianbernhart <fabian_bernhart@web.de>
2025-05-03 23:17:42 +0200

    feat(virtual-dom): add simple virtual dom

--- virtual-dom/vue-vdom.js
+++ virtual-dom/vue-vdom.js
@@ -2,15 +2,39 @@ export function h(tag, props, children) {
   return { tag, props, children };
 }
 
-export function unmount() {}
+export function unmount(vnode) {
+  vnode.el.parentNode.removeChild(vnode.el);
+}
 export function mount(vnode, container) {
   const el = document.createElement(vnode.tag);
+  vnode.el = el;
 
-  el.textContent = vnode.children;
-  el.className = vnode.props.class;
+  for (const key in vnode.props) {
+    if (key.startsWith("on")) {
+      const eventName = key.slice(2).toLowerCase();
+
+      el.addEventListener(eventName, vnode.props[key]);
+    } else {
+      el.setAttribute(key, vnode.props[key]);
+    }
+  }
+
+  if (typeof vnode.children === "string") {
+    el.textContent = vnode.children;
+  } else if (Array.isArray(vnode.children)) {
+    vnode.children.forEach((child) => mount(child, el));
+  } else {
+    mount(vnode.children, el);
+  }
 
   container.appendChild(el);
-
-  console.log("mount", vnode, container);
 }
-export function patch() {}
+export function patch(vnode1, vnode2) {
+  if (typeof vnode2.children === "string") {
+    vnode1.el.textContent = vnode2.children;
+  } else {
+    for (let i = 0; i < vnode1.children.length; i++) {
+      patch(vnode1.children[i], vnode2.children[i]);
+    }
+  }
+}
