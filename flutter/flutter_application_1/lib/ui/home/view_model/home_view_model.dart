5d2dcb01a3435aecf37e92b1ea9f8f0168866507
fabianbernhart <fabian_bernhart@web.de>
2025-05-14 22:08:34 +0200

    feat: add home view model and home screen

--- flutter/flutter_application_1/lib/ui/home/view_model/home_view_model.dart
+++ flutter/flutter_application_1/lib/ui/home/view_model/home_view_model.dart
@@ -1,1 +1,12 @@
-// ViewModel class for home
+import 'package:flutter/material.dart';
+
+import '../../../routing/routes.dart';
+
+class HomeViewModel extends ChangeNotifier {
+  String title = 'Home';
+  String description = 'Welcome to the Home Screen!';
+
+  String getStartedText = 'Get Started';
+
+  String copyRoute = Routes.copy;
+}
--- flutter/flutter_application_1/lib/ui/home/widgets/home_screen.dart
+++ flutter/flutter_application_1/lib/ui/home/widgets/home_screen.dart
@@ -1,1 +1,35 @@
-// Main screen for home
+import 'package:flutter/material.dart';
+
+import '../view_model/home_view_model.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key, required this.viewModel});
+
+  final HomeViewModel viewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(viewModel.title)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              viewModel.title,
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+            Text(
+              viewModel.description,
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            FilledButton(
+              onPressed: () {},
+              child: Text(viewModel.getStartedText),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
