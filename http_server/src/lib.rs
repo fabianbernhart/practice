a5822b2eb8fc2608bfa95ee3bc63e070d82f9324
fabianbernhart <fabian_bernhart@web.de>
2025-05-28 23:13:51 +0200

    chore(fmt): http_server

--- http_server/src/lib.rs
+++ http_server/src/lib.rs
@@ -10,11 +10,11 @@ pub struct ThreadPool;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
-    /// 
+    ///
     /// The size is the number of threads in the pool.
-    /// 
+    ///
     /// # Panics
-    /// 
+    ///
     /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0, "The size of pools needs to be bigger than 0");
